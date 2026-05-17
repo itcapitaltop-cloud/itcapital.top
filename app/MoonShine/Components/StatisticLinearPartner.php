1d0105f802f509fab25ff8cc18a3d4a14bec036d
odanilyuk <next-t3ch@yandex.ru>
2026-05-17 15:11:24 +0300

    fix

--- app/MoonShine/Components/StatisticLinearPartner.php
+++ app/MoonShine/Components/StatisticLinearPartner.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\MoonShine\Components;
 
 use App\Actions\User\Partner\ProgressBarAction;
+use App\Models\PartnerRank;
 use App\Models\User;
 use MoonShine\Components\MoonShineComponent;
 
@@ -25,8 +26,11 @@ final class StatisticLinearPartner extends MoonShineComponent
      */
     protected function viewData(): array
     {
+        $user = User::query()->findOrFail($this->userId);
+        $maxRank = (int) PartnerRank::query()->max('rank');
+
         return [
-            'nextRank' => User::query()->findOrFail($this->userId)->rank + 1,
+            'nextRank' => $user->rank + 1 <= $maxRank ? $user->rank + 1 : null,
             'progressBars' => ProgressBarAction::make()->run($this->userId),
         ];
     }
